2704c6b7d47bd745999e4a7a5c12f9bd591699b8
bsaw <brendan_saw@sfu.ca>
2021-03-14 22:41:31 -0700

    Created buttons in Flask using POST

--- sendtodb.py
+++ sendtodb.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
+import sys
 
 app = Flask(__name__)
 
@@ -13,15 +14,26 @@ app.config['MYSQL_DB'] = 'cmpt354'
 
 mysql = MySQL(app)
 
-@app.route("/")
-def defaultFunction():
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/home", methods=['GET', 'POST'])
+def home():
     cursor = mysql.connection.cursor()
     cursor.execute('''SELECT * FROM category''')
     retVal = cursor.fetchall()
     cursor.close()
-
-    return f"<h1>Category</h1><p>{str(retVal)}</p>"
-
+    print("Hello", file=sys.stderr)
+    clickValue = "None"
+    if request.method == "POST":
+        print("In POST", file=sys.stderr)
+        #print(request.form.get('submitbutton'))
+        for i in retVal:
+            for j in i:
+                if (request.form['submitbutton'] == str(j)):
+                    clickValue = str(j)
+                    print(j, file=sys.stderr)
+                    return render_template('home.html', homeRetVal = [str(retVal), retVal, clickValue])
+    
+    return render_template('home.html', homeRetVal = [str(retVal), retVal, clickValue])
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
