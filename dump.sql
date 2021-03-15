b0fbf0316fd5fe53b56980d5a2b70628dcb66b88
AndyLu6838 <74114385+AndyLu6838@users.noreply.github.com>
2021-03-14 22:42:12 -0700

    on update/deletes added, triggers added

--- dump.sql
+++ dump.sql
@@ -7,11 +7,7 @@ CREATE TABLE user (
     province VARCHAR(100),
     city VARCHAR(100),
     email VARCHAR(100),
-    PRIMARY KEY (userid),
-    CONSTRAINT uid_valid CHECK (userid>=0),
-    CONSTRAINT hnum_valid CHECK (house_number>=0),
-    CONSTRAINT password_valid CHECK (LENGTH(user_password)>=1),
-    CONSTRAINT email_valid CHECK (Email REGEXP '^[0-9A-Za-z._-]+@[0-9A-Za-z.-]+\.[a-zA-Z0-9-.]+$')
+    PRIMARY KEY (userid)
 );
 
 CREATE TABLE customer (
@@ -20,6 +16,8 @@ CREATE TABLE customer (
     last_name VARCHAR(100),
     PRIMARY KEY (id),
     FOREIGN KEY (id) REFERENCES user(userid)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE seller (
@@ -27,6 +25,8 @@ CREATE TABLE seller (
     company_name VARCHAR(100),
     PRIMARY KEY (id),
     FOREIGN KEY (id) REFERENCES user(userid)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE category (
@@ -41,15 +41,15 @@ CREATE TABLE product (
     best_before_date DATE NOT NULL,
     product_name VARCHAR(100) NOT NULL,
     price DECIMAL(10, 2),
-    sellerid INT,
+    sellerid VARCHAR(20),
     category_name CHAR(100),
     PRIMARY KEY (productid),
-    FOREIGN KEY (sellerid) REFERENCES seller(id),
-    FOREIGN KEY (category_name) REFERENCES category(category_name),
-    CONSTRAINT pid_valid CHECK (productid >= 0),
-    CONSTRAINT stock_valid CHECK (stock >= 0),
-    CONSTRAINT bbdate_valid CHECK (best_before_date > CURRENT_DATE),
-    CONSTRAINT price_valid CHECK (price >= 0.0)
+    FOREIGN KEY (sellerid) REFERENCES seller(id)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE,
+    FOREIGN KEY (category_name) REFERENCES category(category_name)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE store_order (
@@ -59,18 +59,18 @@ CREATE TABLE store_order (
     order_time DATE,
     payment_method_used VARCHAR(100),
     PRIMARY KEY (customerid, orderid),
-    FOREIGN KEY (customerid) REFERENCES customer(id),
-    CONSTRAINT oid_valid CHECK (orderid>=0),
-    CONSTRAINT cost_valid CHECK (cost>=0.0),
-    CONSTRAINT time_valid CHECK (order_time<=CURRENT_DATE)
+    FOREIGN KEY (customerid) REFERENCES customer(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE shopping_cart (
     customerid VARCHAR(20),
     cartid INT,
     PRIMARY KEY (customerid, cartid),
-    FOREIGN KEY (customerid) REFERENCES customer(id),
-    CONSTRAINT cid_valid CHECK (cartid>=0)
+    FOREIGN KEY (customerid) REFERENCES customer(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE product_in_shopping_cart (
@@ -79,17 +79,24 @@ CREATE TABLE product_in_shopping_cart (
     cartid INT,
     quantity INT,
     PRIMARY KEY (productid, customerid, cartid),
-    FOREIGN KEY (productid) REFERENCES product(productid),
-    FOREIGN KEY (customerid, cartid) REFERENCES shopping_cart(customerid, cartid),
-    CONSTRAINT qnty_valid CHECK (quantity>=0)
+    FOREIGN KEY (productid) REFERENCES product(productid)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (customerid, cartid) REFERENCES shopping_cart(customerid, cartid)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE adds (
     customerid VARCHAR(20),
     productid INT,
     PRIMARY KEY (customerid, productid),
-    FOREIGN KEY (customerid) REFERENCES customer(id),
+    FOREIGN KEY (customerid) REFERENCES customer(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
     FOREIGN KEY (productid) REFERENCES product(productid)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE customer_payment_method (
@@ -97,4 +104,75 @@ CREATE TABLE customer_payment_method (
     payment_method VARCHAR(100),
     PRIMARY KEY (customerid, payment_method),
     FOREIGN KEY (customerid) REFERENCES customer(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
+
+DELIMITER $$
+
+CREATE TRIGGER `user_insert_constraints` BEFORE INSERT ON `user`
+FOR EACH ROW  
+IF LENGTH(NEW.userid) < 1 OR NEW.house_number < 0 OR LENGTH(NEW.user_password) < 1 OR NEW.email NOT LIKE '%_@__%.__%' 
+THEN
+    SIGNAL SQLSTATE '45000';
+END IF;$$
+
+CREATE TRIGGER `user_update_constraints` BEFORE UPDATE ON `user`
+FOR EACH ROW IF LENGTH(NEW.userid) < 1 OR NEW.house_number < 0 OR LENGTH(NEW.user_password) < 1 OR NEW.email NOT LIKE '%_@__%.__%' 
+THEN
+    SIGNAL SQLSTATE '45000';
+END IF;$$
+
+CREATE TRIGGER `product_insert_constraints` BEFORE INSERT ON `product`
+FOR EACH ROW IF NEW.productid < 0 OR NEW.stock < 0 OR NEW.best_before_date <= CURRENT_DATE OR NEW.price < 0.0 
+THEN
+    SIGNAL SQLSTATE '45000';
+END IF;$$
+
+CREATE TRIGGER `product_update_constraints` BEFORE UPDATE ON `product`
+FOR EACH ROW IF NEW.productid < 0 OR NEW.stock < 0 OR NEW.best_before_date <= CURRENT_DATE OR NEW.price < 0.0 
+THEN
+    SIGNAL SQLSTATE '45000';
+END IF;$$
+
+CREATE TRIGGER `store_order_insert_constraints` BEFORE INSERT ON `store_order`
+FOR EACH ROW IF NEW.orderid < 0 OR NEW.cost < 0.0 OR NEW.order_time > CURRENT_DATE 
+THEN
+    SIGNAL SQLSTATE '45000';
+END IF;$$
+
+CREATE TRIGGER `store_order_update_constraints` BEFORE UPDATE ON `store_order`
+FOR EACH ROW IF NEW.orderid < 0 OR NEW.cost < 0.0 OR NEW.order_time > CURRENT_DATE 
+THEN
+    SIGNAL SQLSTATE '45000';
+END IF;$$
+
+CREATE TRIGGER `shopping_cart_update_constraint` BEFORE UPDATE ON `shopping_cart`
+FOR EACH ROW IF NEW.cartid < 0 
+THEN
+    SIGNAL SQLSTATE '45000';
+END IF;$$
+
+CREATE TRIGGER `shopping_cart_insert_constraint` BEFORE INSERT ON `shopping_cart`
+FOR EACH ROW IF NEW.cartid < 0 
+THEN
+    SIGNAL SQLSTATE '45000';
+END IF;$$
+
+CREATE TRIGGER `product_in_shopping_cart_insert_constraints` BEFORE INSERT ON `product_in_shopping_cart`
+FOR EACH ROW IF NEW.quantity < 0 
+THEN
+    SIGNAL SQLSTATE '45000';
+END IF;$$
+
+CREATE TRIGGER `product_in_shopping_cart_update_constraints` BEFORE UPDATE ON `product_in_shopping_cart`
+FOR EACH ROW IF NEW.quantity < 0 
+THEN
+    SIGNAL SQLSTATE '45000';
+END IF;$$
+
+
+
+
+
+
